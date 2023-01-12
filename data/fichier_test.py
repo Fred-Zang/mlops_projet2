48df61c4b9fa24425074a48224e91f92e473d583
Zanghi <contact.fredzang@gmail.com>
2023-01-12 21:58:04 +0200

    Create fichier_test.py
    fichier de test python pour lever l'erreur github actions qui a besoin d'un fichier à tester selon PyTest

---
+++ data/fichier_test.py
@@ -0,0 +1,12 @@
+def total(liste):
+  """ renvoie la somme des éléments d'une liste """
+  result : float = 0.0
+  for item in liste:
+    result += item
+  return (result)
+
+
+def test_total():
+  assert(total([1.0, 2.0, 3.0])) == 6.0
+  
+  
