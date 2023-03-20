118032f2f34cbe87443ebdca91aaae58cba6c588
Quan LIU <75064362+luckychien87@users.noreply.github.com>
2023-03-20 02:15:18 +0100

    add upload file endpoint
    upload file endpoint : upload csv file, save the file with data time, update the global csv
    clean some codes
    change the data repo to airflow/clean_data
    all endpoints are tested

--- fastapi/PyAPI.py
+++ fastapi/PyAPI.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 """
 Created on Fri Dec 30 01:41:01 2022
+
 @author: user
 """
-from fastapi import Depends, FastAPI, HTTPException, status, Query
+from fastapi import Depends, FastAPI, HTTPException #, status, Query
+from fastapi import File, UploadFile
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from passlib.context import CryptContext
 from pydantic import BaseModel
@@ -12,6 +14,7 @@ import pandas as pd
 import numpy as np
 import csv 
 import json
+import datetime
 
 app = FastAPI(
     title="Backend FastAPI by Eric & Fred & Quan",
@@ -59,10 +62,13 @@ On récupère l'identifiant et le mot de passe de l'utilisateur grâce aux attri
 On vérifie si l'identifiant est présent dans la base de données. Ensuite, on compare si le mot de passe crypté correspond bien à \
 celui de la base de données en utilisant la méthode verify de la variable pwd_context.  \
 Sinon, on renvoie l'identifiant de l'utilisateur.
+
     Args:
         credentials (HTTPBasicCredentials, optional): _description_. Defaults to Depends(security).
+
     Raises:
         HTTPException: On lève une erreur 401 si username et hashedpassword ne correspondent pas
+
     Returns:
         credentials.username
     """
@@ -92,45 +98,17 @@ async def get_status(username: str = Depends(get_current_user)):
 async def current_user(username: str = Depends(get_current_user)):
     """
     _summary_ : pour accéder à cette route, il faut au préalable que l'utilisateur se soit authentifié.
+
         HELLO user
+
     Args: username (str, optional): _description_. Defaults to Depends(get_current_user).
+
     Returns: _type_: string  _description_: affiche "Hello" + "username"
     """
     return "Hello {}".format(username)
 
 
 # ----------------------------------2eme route Création de la base de donnée ------------------------------------------------ #
-
-
-# @app.post("/Postez un commentaire",
-#           name="Nouveau Commentaire ",
-#           tags=['admin'])
-# async def admin_user(comment: str, username: str = Depends(get_current_user)):
-#     """
-#     _summary_ : détails à donner
-
-
-#     Args: idem
-#     """
-
-#     # ? ajouter la date de saisie du commentaire en return
-#     return {username: comment}
-
-# # original data set
-# #Commentaire	star	date	client	reponse	source	company	ville	maj	date_commande	ecart
-# class Item(BaseModel):
-#     # inID: Optional[int] = None 
-#     Commentaire: str
-#     star : int
-#     date: Optional[str] = None
-#     client: Optional[str]= None
-#     reponse: Optional[str]= None
-#     source: Optional[str] = None
-#     company: Optional[str] = None
-#     ville: Optional[str]=None
-#     maj: Optional[str]=None
-#     date_commande: Optional[str] = None 
-#     ecart: Optional[int]=None
 
 # # data used in GBC, ANN and SVM 
 class Item(BaseModel):
@@ -143,25 +121,16 @@ class Item(BaseModel):
     Star_org: Optional[int] =None
     date: Optional[str] = None
 
-# data base  
-data_store = '/airflow/clean_data/data_MAJ.csv'
+# data base  information
+########################################
+# modify this adresse to airflow
+data_store_path = '../airflow/clean_data/'
+# modify this file to origin database 
+# => data_MAJ.csv
+# this is the test small data base
+data_name = 'data_MAJ2.csv'
+########################################  
 
-# function definition
-# function to store a new comment
-# def write_comment(FileName : str, inputs:list):
-#     with open(FileName,'a+') as csvfile:
-#         writer = csv.writer(csvfile)
-#         writer.writerow([inputs.Commentaire, 
-#         inputs.star, 
-#         inputs.date, 
-#         inputs.client, 
-#         inputs.reponse, 
-#         inputs.source, 
-#         inputs.company,
-#         inputs.ville,
-#         inputs.maj,
-#         inputs.date_commande,
-#         inputs.ecart])
 
 def write_comment(FileName : str, inputs:list):
     with open(FileName,'a+') as csvfile:
@@ -176,28 +145,101 @@ def write_comment(FileName : str, inputs:list):
         inputs.Star_org,
         inputs.date])
 
-
-
-
 @app.post('/comment', name="Make a New comment", tags= ['Data Management'])
 async def post_comment(item:Item):
     # data storage file name
-    data_store = '/airflow/clean_data/data_MAJ.csv' # change name data_MAJ.csv
-
-    write_comment(data_store,item)
-    comments = pd.read_csv(data_store)
+    ########################################
+    # modify this adresse to airflow
+    data_store_path = '../airflow/clean_data/'
+    # modify this file to origin database
+    # => data_MAJ.csv
+    # this is a small test data base
+    data_name = 'data_MAJ2.csv'
+    ########################################  
+    
+    
+    data_file = data_store_path+data_name
+    print(data_file)
+    write_comment(data_file,item)
+    # modify the InID 
+    comments = pd.read_csv(data_file)
+    comments['inID']= range(len(comments))
+    # comments.iloc[-1,0]=comments.iloc[-2,0]+1
+    comments.to_csv(data_file,index=False) 
     comment = comments.iloc[-1,:]
-    return {'new comment': comment['Commentaire']}
+    
+    return {'last comment ID': int(comment['inID']),'last comment': comment['Commentaire']}
+    
+
 
 @app.get('/getcomments', name= 'Get comments', tags = ['Data Management'])
 def get_comments():
+    """"
+    return comment data base by json
+
+    """
     # data storage file name
-    data_store = '/airflow/clean_data/data_MAJ.csv'
-    comments = pd.read_csv(data_store)
-    # comment = comments.iloc[-1,:]
-    # total = len(comments.Commentaire)
-    data_json = comments.to_json('/airflow/clean_data/data_MAJ.json')
+    ########################################
+    # modify this adresse to airflow
+    data_store_path = '../airflow/clean_data/'
+    # modify this file to origin database
+    # => data_MAJ.csv
+    # this is a small database
+    data_name = 'data_MAJ2.csv'
+    data_file_csv = data_store_path+data_name
+    # modify the json name
+    # => data_MAJ.json
+    data_file_json = data_store_path + 'data_MAJ2.json'
+    ########################################  
+    comments = pd.read_csv(data_file_csv)
+
+    data_json = comments.to_json(data_file_json)
 
     data = comments.to_json()
     
     return data
+
+
+@app.post("/uploadfile/", name ='upload file', tags = ['Data Management'])
+async def create_upload_file(file: UploadFile):
+    """
+    the uploaded file is saved with current datatime
+    """
+
+    ########################################
+    # modify this adresse to airflow
+    data_store_path = '../airflow/clean_data/'
+    # modify this file to origin database
+    # => data_MAJ.csv
+    # this is a small test data base
+    data_origin = 'data_MAJ2.csv'
+    
+    ######################################## 
+
+    print(file.content_type)
+    
+    data_file = data_store_path + data_origin
+    dt = datetime.datetime.now()
+    # if the hour and minute cause traitement problem, we can keep only y-m-d
+    time = dt.strftime('%Y-%m-%d-%H:%M')
+
+    file.filename = 'new_data_'+ time + '.csv'
+    if file.content_type == "text/csv": 
+        # write into a new data file
+        with open(data_store_path + file.filename,'wb') as f:
+            f.write(await file.read())
+
+        # update the data_MAJ file (global data base)
+        added_data = pd.read_csv(data_store_path + file.filename)
+        added_data.to_csv(data_file, mode = 'a',header = False, index = False)
+        
+        # modify the ID
+        data = pd.read_csv(data_file)
+        data['inID'] = range(len(data))
+        data.to_csv(data_file,index = False)
+        return {"filename": file.filename}
+    else:
+    # elif file.content_type =='application/json':
+        return {'INFO':'please use a csv file'}
+   
+
