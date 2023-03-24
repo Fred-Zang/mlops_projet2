52a06bfcc82c0b30dfb3bf0d20985b2e4f7a9f6f
fredzang <fred07@PCwikHap.localdomain>
2023-03-24 21:30:14 +0100

    MAJ comment airflow.py

--- airflow/dags/airflow_molps_projet.py
+++ airflow/dags/airflow_molps_projet.py
@@ -6,7 +6,7 @@ from airflow.utils.dates import days_ago
 
 
 import sys
-sys.path.append('/app/clean_functions')
+sys.path.append('/app/clean_functions')   # chemin /airflow/clean_functions error => on passe par le volume /app
 from my_models import GBC_predict_df, SVM_predict_df, ANN_predict_df
 from my_functions import token_lemmatiser
 
@@ -28,7 +28,7 @@ my_dag = DAG(
     * Save the models
     """,
     tags=['projet', 'mlops'],
-    schedule_interval=None,  # '* * * * *',   # 10 minutes
+    schedule_interval=None,  # '* * * * *',
     default_args={
         'owner': 'airflow',
         'start_date': days_ago(0, minute=1),
@@ -36,9 +36,9 @@ my_dag = DAG(
     catchup=False)
 
 
-#####################################################
-#   0. PRE-PREOCESSING DE DATA_MAJ.CSV              #
-#####################################################
+####################################################################################
+#   0. PRE-PREOCESSING DE DATA_MAJ.CSV  => CREATION DE DATA_PREPROCESSING_V1.CSV   #
+####################################################################################
 def Data_Preprocessing():
     token_lemmatiser(path="/app/clean_data/data_MAJ.csv")
 
@@ -79,10 +79,10 @@ task1 = PythonOperator(
 task2 = PythonOperator(
     task_id="preprocess_2_train_save_GBC2_save_classif",
     doc_md="""
-    ## Training and Saving of the GBC2 model
+    ## Preprocessing & Training & Saving of the GBC2 model
      + calculating and saving Classification_report on Jason file
-    * Input: '../clean_data/data_preprocess_v1.csv'
-    * Output: '../clean_model/GBC2_sav-DAG.pickle'
+    * Input: Input: data_MAJ.csv = data originale sans colonnes de preprocessing 
+    * Output: '/clean_model/GBC2_sav-DAG.pickle'
     """,
     python_callable=GBC_preprocess_train_predict,
     dag=my_dag)
@@ -94,8 +94,8 @@ task3_1 = PythonOperator(
     doc_md="""
     ## Training and Saving of the SVM model
      + calculating and saving Classification_report on Jason file
-    * Input: '../clean_data/data_preprocess_v1.csv'
-    * Output: '../clean_model/SVM-updated.pickle'
+    * Input: '/clean_data/data_preprocess_v1.csv'
+    * Output: '/clean_model/SVM-updated.pickle'
     """,
     python_callable=SVM_predict,
     dag=my_dag)
@@ -107,8 +107,8 @@ task3_2 = PythonOperator(
     doc_md="""
     ## Training and Saving of the ANN model.pickle
      + calculating and saving Classification_report on Jason file
-    * Input: '../clean_data/review_trust_fr_lemmantiser_word+2_VF.csv'
-    * Output: '../clean_model/ANN-updated.h5'
+    * Input: '/clean_data/'/clean_data/data_preprocess_v1.csv''
+    * Output: '/clean_model/ANN-updated.h5'
     """,
     python_callable=ANN_predict,
     dag=my_dag)
