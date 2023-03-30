3723a0752d25a87ae73581c46e31c42a968d4730
egasnier <gasniere.th@gmail.com>
2023-03-30 17:06:46 +0200

    MAJ2 avec passage du pep8 et suppression de #

--- airflow/clean_functions/my_models.py
+++ airflow/clean_functions/my_models.py
@@ -41,13 +41,13 @@ sys.path.append('/app/clean_functions')
 #####################################################
 #   1. MODELE GBC - GRADIENT BOOSTING CLASSIFIER    #
 #####################################################
-def GBC_predict_df(path_data,
-                   path_stopwords='/app/clean_data/liste_no-stop-words_tokens_unique.xlsx',
-                   save_model=True,
-                   path_save_model='/app/clean_model/GBC_2-sav_DAG.pickle',
-                   save_vectorizer=True,
-                   path_save_vectorizer='/app/clean_model/vectoriser_GBC_2-sav_DAG'
-):
+def GBC_predict_df(
+        path_data,
+        path_stopwords='/app/clean_data/liste_no-stop-words_tokens_unique.xlsx',
+        save_model=True,
+        path_save_model='/app/clean_model/GBC_2-sav_DAG.pickle',
+        save_vectorizer=True,
+        path_save_vectorizer='/app/clean_model/vectoriser_GBC_2-sav_DAG'):
     """Prédiction avec un Gradient Boosting Classifier:
             1.1 Préparation des données
             1.2 Entrainement du modèle
@@ -62,13 +62,13 @@ def GBC_predict_df(path_data,
         path_save_model: Chemin de sauvegarde du modèle GBC
         save_vectorizer: Boolean indiquant si on sauve le vectorizer ou pas
         path_save_vectorizer: Chemin de stockage du vectorizer
-    
+
     Returns:
     --------
         les prédictions
     """
 
-    ##### 1.1 PREPARATION DES DONNES #####
+    # 1.1 PREPARATION DES DONNES
     # ------------------------------------
     # Importer les données nettoyées
     df = pd.read_csv(path_data, index_col=0)
@@ -99,7 +99,7 @@ def GBC_predict_df(path_data,
 
     # print(vectorizer.vocabulary_)
 
-    ##### 1.2 ENTRAINEMENT DU MODELE #####
+    # 1.2 ENTRAINEMENT DU MODELE
     # ------------------------------------
     GBC_2 = GradientBoostingClassifier(  # The number of estimators as selected by early stopping
         n_estimators=100,
@@ -113,7 +113,7 @@ def GBC_predict_df(path_data,
         random_state=0
     ).fit(X_train_GBC_2, y_train)
 
-    ##### 1.3 SAUVEGARDE DU MODELE #####
+    # 1.3 SAUVEGARDE DU MODELE
     # ----------------------------------
     # sauvegarder le modèle pré-entrainé
     if save_model:
@@ -122,7 +122,7 @@ def GBC_predict_df(path_data,
     if save_vectorizer:
         pickle.dump(vectorizer, open(path_save_vectorizer, 'wb'))
 
-    ##### 1.4 EVALUATION DU MODELE #####
+    # 1.4 EVALUATION DU MODELE
     # ----------------------------------
     # Calculer les prédictions
     y_pred_GBC_2 = GBC_2.predict(X_test_GBC_2)
@@ -138,7 +138,6 @@ def GBC_predict_df(path_data,
     out_file.close()
 
 
-
 #####################################################
 #       2.1 PASSAGE DU CORPUS SUR UN MODELE         #
 #            PRE-ENTRAINE EN LANGUE FRANCAISE       #
@@ -146,20 +145,21 @@ def GBC_predict_df(path_data,
 #            DE 2.500.733 MOTS VECTORISES           #
 #####################################################
 
-# ### Chargement du modèle pré-entrainé de wikipedia2vec
+# Chargement du modèle pré-entrainé de wikipedia2vec
 # - TUTO d'aide pour charger une réprésentation pré-entraînée dans une langue souhaitée de wikipedia2vec
 #
 #    - 1 aller sur le lien @  https://wikipedia2vec.github.io/wikipedia2vec/pretrained/#french
 #    - 2 copier le lien d'adresse d'un des 4 fichiers souhaité en langue Française de la ligne
 #      frwiki_20180420 (window=5, iteration=10, negative=15): 100d (bin) 100d (txt) 300d (bin) 300d (txt)
 #    - 3 placer le lien sur notre IDE spyder ou jupyter
-#    - 4 avec la souris sur ce chemin, faire CRL + clic => le fichier se télécharge = 1h pour le + petit de 450Mo nommé 100d(txt)
+#    - 4 avec la souris sur ce chemin, faire CRL + clic
+#               => le fichier se télécharge = 1h pour le + petit de 450Mo nommé 100d(txt)
 #    - 5 placer ce fichier dans un dossier et le décompresser => 1.9Go
-#    - 6 placer ce fichier décompressé dans le dossier C:\\Users\\UserPC\\anaconda3\\envs\\spyder-env\\lib\\site-packages\\gensim\\test\\test_data\\
+#    - 6 placer ce fichier décompressé dans le dossier
+#           C:\\Users\\UserPC\\anaconda3\\envs\\spyder-env\\lib\\site-packages\\gensim\\test\\test_data\\
 #      où spyder-env est mon environnement spécial spyder (pas obligatoire)
 #    - 7 enfin lancer la cellule ci-dessous pour charger le fichier sur notre IDE et créer un vecteur
 #
-
 # Le chargement d'un objet gensim prend 6 à 8 mins chaque fois, le modèle **trained** est sauvegardé et utilisé dans la suite
 #
 # si c'est la première fois et que vous n'avez pas récupéré le
@@ -172,7 +172,8 @@ trained = load_word2vec_format('frwiki_20180420_100d.txt.bz2', binary=False)    
 
 end = time()
 print("temps chargement modele pré-entrainé = ", end-start)
-# à voir absolument le parametre unicode_errors='ignore' ou 'replace' qui sera utile par la suite pour éviter des ereurs en chargement
+# à voir absolument le parametre unicode_errors='ignore' ou 'replace' qui sera utile par la suite
+# pour éviter des ereurs en chargement
 
 print(trained.vectors.shape)
 '''
@@ -185,11 +186,11 @@ print(trained.vectors.shape)
 #####################################################
 
 
-def SVM_predict_df(path_data='/app/clean_data/review_trust_fr_lemmantiser_word+2_VF.csv',
-                   path_model_wiki='/app/clean_model/trained.pickle',
-                   save_model=True,
-                   path_save_model='/app/clean_model/SVM_sav-DAG.pickle'
-):
+def SVM_predict_df(
+        path_data='/app/clean_data/review_trust_fr_lemmantiser_word+2_VF.csv',
+        path_model_wiki='/app/clean_model/trained.pickle',
+        save_model=True,
+        path_save_model='/app/clean_model/SVM_sav-DAG.pickle'):
     """Prédiction avec un Support Vector Machines:
             2.2.1 Préparation des données
             2.2.2 Entrainement du modèle
@@ -210,7 +211,7 @@ def SVM_predict_df(path_data='/app/clean_data/review_trust_fr_lemmantiser_word+2
     # 2.2.1 PRETRAITEMENT DU CORPUS ET CREATION DES JEUX D'ENTRAINEMENT ET
     X_train, X_test, y_train, y_test = prepare_data(path_data, path_model_wiki)
 
-    ##### 2.2.2 ENTRAINEMENT DU MODELE #####
+    # 2.2.2 ENTRAINEMENT DU MODELE
     # SVM avec noyau RBF par défaut
     SVM = SVC(C=100, gamma=1, random_state=0)
     # Ces paramètres ont été trouvés par GridSearchCV sur quelques valeurs seulement
@@ -232,12 +233,12 @@ def SVM_predict_df(path_data='/app/clean_data/review_trust_fr_lemmantiser_word+2
     # print("grid.best_params :", grid.best_params_)
     # svm_best= grid.best_estimator_
 
-    ##### 2.2.3 SAUVEGARDE DU MODELE #####
+    # 2.2.3 SAUVEGARDE DU MODELE
     # ------------------------------------
     if save_model:
         pickle.dump(SVM, open(path_save_model, 'wb'))
 
-    ##### 2.2.4 EVALUATION DU MODELE #####
+    # 2.2.4 EVALUATION DU MODELE
     # ------------------------------------
     # Calcul des prédictions
     y_pred_SVM = SVM.predict(X_test)
@@ -253,7 +254,6 @@ def SVM_predict_df(path_data='/app/clean_data/review_trust_fr_lemmantiser_word+2
     out_file.close()
 
 
-
 #####################################################
 #    2.3 MODELE ANN ARTIFICEL NEURONAL NETWORK      #
 #####################################################
@@ -261,7 +261,7 @@ def ANN_predict_df(path_data,
                    path_model_wiki='/app/clean_model/trained.pickle',
                    save_model=True,
                    path_save_model='/app/clean_model/ANN-sav-DAG.h5',
-):
+                   ):
     """Prédiction avec un réseau de neurones:
             2.3.1 Préparation des données
             2.3.2 Création du modèle
@@ -283,7 +283,7 @@ def ANN_predict_df(path_data,
     # 2.2.1 PRETRAITEMENT DU CORPUS ET CREATION DES JEUX D'ENTRAINEMENT ET
     X_train, X_test, y_train, y_test = prepare_data(path_data, path_model_wiki)
 
-    ##### 2.2.2 CREATION DU MODELE #####
+    # 2.2.2 CREATION DU MODELE
     inputs = Input(shape=(100))
     dense_1 = Dense(units=64, activation="relu")
     dropout_2 = Dropout(rate=0.1)
@@ -330,7 +330,7 @@ def ANN_predict_df(path_data,
         # To restore the weights of the best epoch
         restore_best_weights=True)
 
-    ##### 2.2.3 ENTRAINEMENT DU MODELE #####
+    # 2.2.3 ENTRAINEMENT DU MODELE
     BATCH_SIZE = 128
 
     ANN.fit(X_train, y_train,
@@ -348,12 +348,12 @@ def ANN_predict_df(path_data,
             # Using all processors
             workers=-1)
 
-    ##### 2.2.4 SAUVEGARDE DU MODELE #####
+    # 2.2.4 SAUVEGARDE DU MODELE
     # ------------------------------------
     if save_model:
         ANN.save(path_save_model)
 
-    ##### 2.2.5 EVALUATION DU MODELE #####
+    # 2.2.5 EVALUATION DU MODELE
     # ------------------------------------
     # Calcul des prédictions
     # => dim (5160, 2) où n =dim[0]de X_test et 2 = dim units dense_last
